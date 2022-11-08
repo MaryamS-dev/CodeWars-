8fbb517f639a8dec4ab02f1671712870ed13784e
maryams7290 <maryamsohail2@gmail.com>
2022-11-08 15:15:05 -0600

    added 3:14 11-8

--- CodeWars/11-5-22.js
+++ CodeWars/11-5-22.js
@@ -25,3 +25,36 @@ function greet (name, owner) {
       return("Hello guest")
     }
   }
+
+/*
+  Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+
+Example
+['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+*/
+
+function smash (words) {
+  if (words.length===0){
+    return ""
+  } else {
+    return words.join(" ")
+  }
+};
+
+/* You were camping with your friends far away from home, but when it's time to go back, you realize that your fuel is running out and the nearest pump is 50 miles away! You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left.
+
+Considering these factors, write a function that tells you if it is possible to get to the pump or not.
+
+Function should return true if it is possible and false if not. */
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+  if (mpg*fuelLeft >= distanceToPump) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+getGas (50, 25, 2)
+getGas (100, 50, 1)
+
